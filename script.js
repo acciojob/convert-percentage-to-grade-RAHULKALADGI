aa163eadcaee0014e3872b2d7da4e2a4f1463442
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-08-30 14:46:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function calculateGrade(percentage) {
   //your code here
+	percentage = Math.round(percentage);
+	if(percentage>=90) {
+		return 'A';
+	}
+	else if(percentage>=80 && percentage<=89) {
+		return 'B';
+	}
+	else if(percentage>=70 && percentage<=79) {
+		return 'C';
+	}
+	else if(percentage>=60 && percentage<=69) {
+		return 'D';
+	}
+	else {
+		return 'F';
+	} 
 }
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
